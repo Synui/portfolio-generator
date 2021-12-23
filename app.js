0d04fdd54ff829afaf1631ff917e12d9868b9929
Jonathon Michael Schoenholz <92822589+Synui@users.noreply.github.com>
2021-12-23 15:07:22 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-feature/questions
 const inquirer = require('inquirer');
 // const fs = require('fs');
 // const generatePage = require('./src/page-template');
@@ -147,18 +146,3 @@ promptUser()
   .then(portfolioData => {
     console.log(portfolioData);
   });
-=======
-const fs = require('fs');
-
-const generatePage = require('./src/page-template.js');
-
-const profileDataArgs = process.argv.slice(2);
-
-const [name, github] = profileDataArgs;
-
-fs.writeFile('./index.html', generatePage(name, github), err => {
-  if (err) throw new Error(err);
-
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
-develop
